9aa6d2c4ed507a2d36588ef96aff81b0695bf7d9
Chad <chad.doebelin@gmail.com>
2023-04-19 20:33:37 -0500

    updated the requirements.txt for pip and environment.yml for conda

--- chat.py
+++ chat.py
@@ -1,8 +1,30 @@
+"""
+This module implements a chatbot using StableLM, an AI language model trained by StabilityAI.
+The chatbot provides responses to user questions using StableLM's natural language processing capabilities.
+
+The chatbot uses StableLM's pre-trained model, and generates responses based on user inputs using the 
+model's predictive abilities. The chatbot is designed to respond to a variety of questions related to 
+general knowledge topics.
+
+Usage:
+    Run this module directly to start a conversation with the chatbot. You can ask the chatbot any 
+    question and it will generate a response based on its trained knowledge. 
+
+    You can also import this module and use the `oracle_response` function to generate responses to 
+    user questions programmatically.
+
+Author:
+    Chad Doebelin
+"""
+MODEL_NAME = "stabilityai/stablelm-tuned-alpha-7b"
+MAX_LENGTH = 128
+TEMPERATURE = 0.7
+
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, StoppingCriteria, StoppingCriteriaList
 
-tokenizer = AutoTokenizer.from_pretrained("stabilityai/stablelm-tuned-alpha-7b")
-model = AutoModelForCausalLM.from_pretrained("stabilityai/stablelm-tuned-alpha-7b")
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
 
 #tokenizer = AutoTokenizer.from_pretrained("StabilityAI/stablelm-base-alpha-7b")
 #model = AutoModelForCausalLM.from_pretrained("StabilityAI/stablelm-base-alpha-7b")
@@ -32,7 +54,7 @@ def oracle_response(question):
     # add your logic here to generate a response based on the user's question
     prompt = f"{session}\n<|USER|>{question}\n<|ASSISTANT|>\n"
     inputs = tokenizer(prompt, return_tensors="pt").to("cuda")
-    tokens = model.generate(**inputs, max_new_tokens=128,temperature=0.7,do_sample=True, stopping_criteria=StoppingCriteriaList([StopOnTokens()]))
+    tokens = model.generate(**inputs, max_new_tokens=MAX_LENGTH,temperature=TEMPERATURE,do_sample=True, stopping_criteria=StoppingCriteriaList([StopOnTokens()]))
     response = (tokenizer.decode(tokens[0], skip_special_tokens=True))
     return response
 
